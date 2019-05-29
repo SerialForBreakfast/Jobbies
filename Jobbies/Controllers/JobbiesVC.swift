4d53a2439fda0da06b05a9ef7be0e7d6682b51df
Joe McCraw <joe@showblender.com>
2019-05-29 11:07:58 -0700

    Checkmark feature

--- Jobbies/Controllers/JobbiesVC.swift
+++ Jobbies/Controllers/JobbiesVC.swift
@@ -27,5 +27,16 @@ class JobbiesVC: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
