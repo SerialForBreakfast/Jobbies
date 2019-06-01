f272c27b71df30d491f2526e5ccc287f0d22210e
Joe McCraw <joe@showblender.com>
2019-06-01 13:38:14 -0700

    Realm Saved Data working

---
+++ Jobbies/Model/Data.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Jobbies
+//
+//  Created by Joseph McCraw on 6/1/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+    
+    
+}
