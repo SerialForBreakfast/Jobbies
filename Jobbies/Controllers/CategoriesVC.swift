d737af5b48ca420fbe6320be7f4e5404c0676801
Joe McCraw <joe@showblender.com>
2019-06-03 10:16:22 -0700

    Realm CRU working

--- Jobbies/Controllers/CategoriesVC.swift
+++ Jobbies/Controllers/CategoriesVC.swift
@@ -84,6 +84,7 @@ class CategoriesVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
+        
 ////        saveCategories()
 //        tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "goToItems", sender: self)
